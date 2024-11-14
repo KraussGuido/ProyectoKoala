c56584a878437617f84083fcb18539ed410e10bf
Krauss Guido <kraussguido1@gmail.com>
2024-11-13 22:32:11 -0300

    cambios de estetica y algunos arreglos en javascript

---
+++ prod.js
@@ -0,0 +1,44 @@
+const productos = [
+    {
+        id:1,
+        nombre:"cafe",
+        img: "./img/productos/prod1.jpeg",
+        precio: 100,
+        cantidad:0,
+    },
+    {
+        id:2,
+        nombre:"pan casero",
+        img: "./img/productos/prod2.jpeg",
+        precio: 150,
+        cantidad:0,
+    },
+    {
+        id:3,
+        nombre:"merienda",
+        img: "./img/productos/prod3.jpeg",
+        precio: 180,
+        cantidad:0,
+    },
+    {
+        id:4,
+        nombre:"bizcocho de grasa",
+        img: "./img/productos/prod4.jpeg",
+        precio: 250,
+        cantidad:0,
+    },
+    {
+        id:5,
+        nombre:"tortas",
+        img: "./img/productos/prod5.jpeg",
+        precio: 300,
+        cantidad:0,
+    },
+    {
+        id:6,
+        nombre:"medialunas",
+        img: "./img/productos/prod6.jpeg",
+        precio: 1000,
+        cantidad:0,
+    },
+]
